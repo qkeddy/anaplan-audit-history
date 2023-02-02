eb846441c840a63c4d4d91daf8cfafeb5e9cbf31
Quin Eddy <qkeddy@gmail.com>
2023-02-02 15:59:55 -0500

    Added clear console and logging components

--- anaplan.py
+++ anaplan.py
@@ -15,5 +15,29 @@ import threading
 import time
 import argparse
 
+
+# === Clear Console ===
+if os.name == "nt":
+	os.system("cls")
+else:
+	os.system("clear")
+	
+
+# === Setup Logger ===
+# Dynamically set logfile name based upon current date.
+log_file_path = "./"
+local_time = time.strftime("%Y%m%d", time.localtime())
+log_file = log_file_path + local_time + "-ANAPLAN-RUN.LOG"
+log_file_level = logging.INFO  # Options: INFO, WARNING, DEBUG, INFO, ERROR, CRITICAL
+logging.basicConfig(filename=log_file,
+                    filemode='a',  # Append to Log
+                    format='%(asctime)s  :  %(levelname)s  :  %(message)s',
+                    level=log_file_level)
+
+
+logging.info("************** Logger Started ****************")
+
+
+
 AuthToken.Auth.access_token = "test value"
 AuthToken.Auth.refresh_token = "test value"
