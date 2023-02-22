ffb526a3f9f66a72d7233d74b0e60ad585f97e30
Quin Eddy <qkeddy@gmail.com>
2023-02-22 14:21:33 -0500

    Updated the activity_events.csv Stubbed out new functions for Get Models, Actions, processes, etc Added additional URIs for retrieving additional metadata

--- anaplan.py
+++ anaplan.py
@@ -49,12 +49,30 @@ refresh_token = AnaplanOauth.refresh_token_thread(
 	1, name="Refresh Token", delay=AuthToken.Auth.token_ttl, uri=tokens_uri)
 refresh_token.start()
 
+# Load User Activity Codes
 AnaplanOps.get_usr_activity_codes(database_file=database_file)
+
+# Get Users
 AnaplanOps.get_users(users_uri, database_file)
 
 # Get Workspaces
 AnaplanOps.get_anaplan_paged_data(uri=workspaces_uri, token_type="Bearer ", database_file=database_file,
                                   database_table="workspaces", record_path="workspaces", json_path=['meta', 'paging', 'next'])
+
+# Get Models in all Workspace
+
+# Get Import Actions in all Models in all Workspaces
+
+# Get Export Actions in all Models in all Workspaces
+
+# Get Process in all Models in all Workspaces
+
+# Get Actions in all Models in all Workspaces
+
+
+# Get Import Actions
+AnaplanOps.get_anaplan_paged_data(uri=workspaces_uri, token_type="Bearer ", database_file=database_file,
+                                  database_table="actions", record_path="imports", json_path=['meta', 'paging', 'next'])
 
 # Get Events
 AnaplanOps.get_anaplan_paged_data(uri=audit_events_uri, token_type="AnaplanAuthToken ", database_file=database_file,
