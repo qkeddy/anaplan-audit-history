7c1fa8440485eaf223ff297ef06ed89325b4cb47
Quin Eddy <qkeddy@gmail.com>
2023-02-21 22:29:56 -0500

    Cleaned and rearranged up code Removed DRY code

--- AnaplanOps.py
+++ AnaplanOps.py
@@ -2,7 +2,6 @@
 # Description:    Get Anaplan Users
 # ===============================================================================
 
-
 import logging
 import requests
 import pandas as pd
@@ -12,8 +11,11 @@ import sqlite3
 import AuthToken
 import StatusExceptions
 
-import sys
 
+# ===  Load user activity codes from file  ===
+def get_usr_activity_codes(database_file):
+    df = pd.read_csv('activity_events.csv')
+    update_table(database_file=database_file, table="act_codes", df=df)
 
 
 # ===  Get Users Function  ===
@@ -42,59 +44,42 @@ def get_users(uri, database_file):
         StatusExceptions.process_status_exceptions(res, uri)
 
 
-# ===  Get Audit Events Function  ===
-def get_audit_events(uri, database_file):
+# ===  Get Anaplan Paged Data  ===
+def get_anaplan_paged_data(uri, token_type, database_file, database_table, record_path, json_path):
     get_headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': 'AnaplanAuthToken ' + AuthToken.Auth.access_token
+        'Authorization': token_type + AuthToken.Auth.access_token
     }
+    
     res = None
 
-    try:
-        print(uri)
-        res = requests.get(uri, headers=get_headers)
-
-        data = json.loads(res.text)
-        df = pd.json_normalize(data, record_path='response')
-
-        update_table(database_file=database_file, table="events", df=df)
-
-        logging.info("List of events received")
-
-    except:
-        # Check status codes
-        StatusExceptions.process_status_exceptions(res, uri)
-
-# =========================================
-# ===  POC - Get Audit Events Function  ===
-# =========================================
-def get_audit_events2(uri, database_file, database_table, record_path):
-    get_headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'AnaplanAuthToken ' + AuthToken.Auth.access_token
-    }
-    res = None
-
+    count = 1 
     try:
         # Initial endpoint query
         print(uri)
         res = requests.get(uri, headers=get_headers).json()
         df = pd.json_normalize(res, record_path)
+        total_size = res[json_path[0]][json_path[1]]['totalSize']
 
         while True:
             try:
-                next_uri = res['meta']['paging']['nextUrl']
+                # Find key in json path
+                next_uri = res[json_path[0]][json_path[1]][json_path[2]]
                 
+                # Get the next request
                 print(next_uri)
                 res = requests.get(next_uri, headers=get_headers).json()
+
+                # Normalize data frame
                 df_incremental = pd.json_normalize(res, record_path)
 
                 # Append to the existing Data Frame
                 df = pd.concat([df, df_incremental], ignore_index=True)
+                count +=1
 
             except KeyError:
+                # Stop looping when key cannot be found
                 break
 
             except:
@@ -102,50 +87,15 @@ def get_audit_events2(uri, database_file, database_table, record_path):
                 StatusExceptions.process_status_exceptions(res, uri)
 
         update_table(database_file=database_file, table=database_table, df=df)
-        logging.info("List of events received")
-
+        logging.info(f'{total_size} {database_table} records received with {count} API call(s)')
+        print(f'{total_size} {database_table} records received with {count} API call(s)')
+        
     except:
         # Check status codes
         StatusExceptions.process_status_exceptions(res, uri)
-
-
-# ===  Get Workspaces Events Function  ===
-def get_workspaces(uri, database_file):
-    get_headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'AnaplanAuthToken ' + AuthToken.Auth.access_token
-    }
-    res = None
-
-    try:
-        print(uri)
-        res = requests.get(uri, headers=get_headers)
-
-        data = json.loads(res.text)
-        df = pd.json_normalize(data, record_path='workspaces')
-
-        update_table(database_file=database_file, table="workspaces", df=df)
-
-        logging.info("List of events received")
-
-    except:
-        # Check status codes
-        StatusExceptions.process_status_exceptions(res, uri)
-
-
-def get_usr_activity_codes(database_file):
-    df = pd.read_csv('activity_events.csv')
-    print (df)
-    update_table(database_file=database_file, table="act_codes", df=df)
-
 
 
 def update_table(database_file, table, df):
     connection = sqlite3.Connection(database_file)
     df.to_sql(name=table, con=connection,if_exists='replace', index=False)
     connection.commit()
-
-
-get_usr_activity_codes("audit.db3")
-
--- anaplan.py
+++ anaplan.py
@@ -25,33 +25,41 @@ device_id_uri = settings['get_device_id_uri']
 tokens_uri = settings['get_tokens_uri']
 users_uri = settings['get_users_uri']
 audit_events_uri = settings['get_audit_events_uri']
-audit_workspaces_uri = settings['get_workspaces_uri']
+workspaces_uri = settings['get_workspaces_uri']
 register = args.register
 database_file = "audit.db3"
 AuthToken.Auth.client_id = args.client_id
-if args.token_ttl == "": AuthToken.Auth.token_ttl = int(args.token_ttl)
+if args.token_ttl == "":
+	AuthToken.Auth.token_ttl = int(args.token_ttl)
 
 # If register flag is set, then request the user to authenticate with Anaplan to create device code
 if register:
-	logger.info('Registering the device with Client ID: %s' % AuthToken.Auth.client_id)
+	logger.info('Registering the device with Client ID: %s' %
+	            AuthToken.Auth.client_id)
 	AnaplanOauth.get_device_id(device_id_uri)
 	AnaplanOauth.get_tokens(tokens_uri)
-	
+
 else:
 	print('Skipping device registration and refreshing the access_token')
 	logger.info('Skipping device registration and refreshing the access_token')
 	AnaplanOauth.refresh_tokens(tokens_uri, 0)
 
 # Start background thread to refresh the `access_token`
-refresh_token = AnaplanOauth.refresh_token_thread(1, name="Refresh Token", delay=AuthToken.Auth.token_ttl, uri=tokens_uri)
+refresh_token = AnaplanOauth.refresh_token_thread(
+	1, name="Refresh Token", delay=AuthToken.Auth.token_ttl, uri=tokens_uri)
 refresh_token.start()
 
 AnaplanOps.get_usr_activity_codes(database_file=database_file)
 AnaplanOps.get_users(users_uri, database_file)
-# AnaplanOps.get_audit_events(audit_events_uri, database_file)
-AnaplanOps.get_workspaces(audit_workspaces_uri, database_file)
 
-AnaplanOps.get_audit_events2(audit_events_uri, database_file, database_table="events", record_path="response")
+# Get Workspaces
+AnaplanOps.get_anaplan_paged_data(uri=workspaces_uri, token_type="Bearer ", database_file=database_file,
+                                  database_table="workspaces", record_path="workspaces", json_path=['meta', 'paging', 'next'])
+
+# Get Events
+AnaplanOps.get_anaplan_paged_data(uri=audit_events_uri, token_type="AnaplanAuthToken ", database_file=database_file,
+                                  database_table="events", record_path="response", json_path=['meta', 'paging', 'nextUrl'])
+
 
 # Exit with return code 0
 sys.exit(0)
