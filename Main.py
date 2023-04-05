a614e221aad92aa30a9e75ceb3d9eed499287f3c
Quin Eddy <qkeddy@gmail.com>
2023-04-04 20:36:20 -0400

    Removed `anaplan.py` module.

--- Main.py
+++ Main.py
@@ -10,6 +10,15 @@ import AnaplanOauth
 import Globals
 import AnaplanOps
 
+# TODO add error handling for missing kwargs error
+# TODO Test w/ no network connection
+# TODO Improve requests error handling
+# TODO add execution of a Process
+# TODO option to push all data from SQLite database to Anaplan
+# TODO option to load all data from audit regardless of last incremental run
+# TODO add blank header if there are no records
+
+
 
 def main():
 	# Clear the console
