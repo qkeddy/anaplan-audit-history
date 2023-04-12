926aef4a93f14af69a33dbd251a6d93da442dea5
Quin Eddy <qkeddy@gmail.com>
2023-04-12 18:07:25 -0400

    Minor updates

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import anaplan_ops
 
 # TODO option to push all data from SQLite database to Anaplan
 # TODO notify when there is a warning in the process
-# TODO If `lastRun` is 0, then set `AuditData->tableDrop` to true and clear Anaplan model
-# TODO create clean metadata lists of items in the audit log for files and actions
+# TODO If `lastRun` is 0, clear Anaplan model
+# TODO create clean metadata lists of items in the audit log for files and actions (via select distinct)
 
 
 def main():
