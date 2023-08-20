311d240dea42fd61cab44ff561d627969a9e3c49
Quin Eddy <qkeddy@gmail.com>
2023-08-20 19:48:26 -0400

    Added module to test SCIM API endpoint

--- api_endpoint_test.py
+++ api_endpoint_test.py
@@ -1,24 +1,37 @@
 import requests
 import base64
-
+import json
 
 # Set variables
-url = "https://auth.anaplan.com/token/authenticate"
-username = 'quin.eddy@anaplan.com'
-password = 'Q00l83@ns$3!'
+auth_url = 'https://auth.anaplan.com/token/authenticate'
+get_ws_url = 'https://api.anaplan.com/2/0/workspaces'
+get_users_url = 'https://api.anaplan.com/scim/1/0/v2/Users?startIndex=1&count=5'
+username = 'enter_user_email'
+password = 'enter_password'
 
 # Encode credentials
 encoded_credentials = str(base64.b64encode((f'{username}:{password}'
                                             ).encode('utf-8')).decode('utf-8'))
 
 # Set headers and payload
-headers = {
-    'Authorization': 'Basic ' + encoded_credentials
-}
+headers = { 'Authorization': 'Basic ' + encoded_credentials}
 payload = {}
 
-
 # Login via basic authentication
-response = requests.request("POST", url, headers=headers, data=payload)
-# response = requests.post(url, headers=headers, data=payload)
-print(response.text)
+res = requests.request("POST", url=auth_url, headers=headers, data=payload)
+
+# Isolate access_token
+access_token = json.loads(res.text)['tokenInfo']['tokenValue']
+
+print(access_token)
+
+# Set headers for API calls
+headers = { 'Authorization': 'Bearer ' + access_token }
+
+# Test Bulk API - Get Workspaces
+getWorkspaces = requests.get(url=get_ws_url, headers=headers)
+print(getWorkspaces.text)
+
+# Test SCIM API - Get users
+getUsers = requests.get(url=get_users_url, headers=headers)
+print(getUsers.text)
